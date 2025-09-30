00dc191f93989af4839d19115c223536e350c950
kyr2o <emorjaybuieic@gmail.com>
2025-09-30 08:30:28 -0400

    Update Break Gun.lua

--- 142823291/Break Gun.lua
+++ 142823291/Break Gun.lua
@@ -1,35 +1,18 @@
 local function getGunInstance()
-
     for i, v in next, game.Players:GetPlayers() do
-
         if v.Character and v.Character:FindFirstChild("Gun") then
-
             return v.Character.Gun
-
         end
-
     end
-
 end
 
-
-
 local gun = getGunInstance()
-
 if gun then
-
     local shootGun = gun:FindFirstChild("ShootGun", true)
-
     if shootGun then
-
         repeat
-
             shootGun:InvokeServer(1, 0, "AH2")
-
             task.wait()
-
         until (not shootGun or not shootGun.Parent) or not shootGun:IsDescendantOf(workspace)
-
     end
-
 end
