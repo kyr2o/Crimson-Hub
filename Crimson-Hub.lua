01f2c701c8d2301a48d1c41993babd03e0c251b4
kyr2o <emorjaybuieic@gmail.com>
2025-10-02 08:56:45 -0400

    Update Crimson-Hub.lua

--- Crimson-Hub.lua
+++ Crimson-Hub.lua
@@ -882,7 +882,6 @@ function ui:LoadScripts(scriptLoader)
                     local hlist = Instance.new("UIListLayout", row)
                     hlist.FillDirection = Enum.FillDirection.Horizontal
                     hlist.Padding = UDim.new(0, 10)
-                    hlist.VerticalAlignment = Enum.VerticalAlignment.Top
 
                     for _, scriptData in pairs(remainderScripts) do
                         createScriptButton(row, scriptData.name, scriptData.fn)
@@ -894,17 +893,9 @@ function ui:LoadScripts(scriptLoader)
 
         local grid = Instance.new("UIGridLayout", scriptsPage)
         grid.CellSize = UDim2.new(0, 200, 0, 50)
-        grid.CellPadding = UDim2.new(0, 10, 0, 10)
+        grid.CellPadding = UDim2.new(0, 15, 0, 15)
         grid.SortOrder = Enum.SortOrder.LayoutOrder
-        grid.FillDirection = Enum.FillDirection.Horizontal
         grid.HorizontalAlignment = Enum.HorizontalAlignment.Center
-
-        local function resizeRow()
-            row.Size = UDim2.new(1, 0, 0, math.max(50, grid.AbsoluteContentSize.Y))
-         end
-       resizeRow()
-            
-       grid:GetPropertyChangedSignal("AbsoluteContentSize"):Connect(resizeRow
         for name, executeFunc in pairs(scripts) do
             if name == "Break Gun" or name == "KillAll" then
                 createActionButton(scriptsPage, name, function() executeFunc(true) end)
