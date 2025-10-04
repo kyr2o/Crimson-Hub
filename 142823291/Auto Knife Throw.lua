917a29de73fbfa20332e0fc5baba30130d66f8e8
kyr2o <emorjaybuieic@gmail.com>
2025-10-04 05:49:55 -0400

    Update Auto Knife Throw.lua

--- 142823291/Auto Knife Throw.lua
+++ 142823291/Auto Knife Throw.lua
@@ -17,7 +17,7 @@ local AllowedAnimIds = {
 }
 local AnimGateSeconds = 0.75
 
-local leadAmount = 1.77
+local leadAmount = 1.9
 local rangeGainPerStud = 0.0025
 local ignoreThinTransparency = 0.4
 local ignoreMinThickness = 0.4
